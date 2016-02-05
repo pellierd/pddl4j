d855b9115f4fc3c7329c33274a75229b8205cda2
cedric <cedric.gerard@yeastlab.fr>
2016-02-05 12:31:59 +0100

    refactor encoding packages

--- src/main/java/fr/uga/pddl4j/encoding/IntMatrix.java
+++ src/main/java/fr/uga/pddl4j/encoding/IntMatrix.java
@@ -29,120 +29,122 @@ import java.io.Serializable;
  */
 final class IntMatrix implements Serializable {
 
-	/**
-	 * The integer array used to store the matrix.
-	 */
-	private int[] matrix;
+    /**
+     * The integer array used to store the matrix.
+     */
+    private int[] matrix;
 
-	/**
-	 * The dimension of the matrix.
-	 */
-	private int dimension;
+    /**
+     * The dimension of the matrix.
+     */
+    private int dimension;
 
-	/**
-	 * The size of the matrix.
-	 */
-	private int size;
+    /**
+     * The size of the matrix.
+     */
+    private int size;
 
-	/**
-	 * Create a new n-dimensional matrix with a specified size and dimension.
-	 *
-	 * @param size the size of the matrix.
-	 * @param dimension the dimension of the matrix.
-	 * @throws IllegalArgumentException if size <= 0.
-	 */
-	public IntMatrix(final int size, final int dimension) throws IllegalArgumentException {
-		if (size < 0)
-			throw new IllegalArgumentException("size <= 0");
-		this.matrix = new int[(int) Math.pow(size, dimension)];
-		this.dimension = dimension;
-		this.size = size;
-	}
+    /**
+     * Create a new n-dimensional matrix with a specified size and dimension.
+     *
+     * @param size      the size of the matrix.
+     * @param dimension the dimension of the matrix.
+     * @throws IllegalArgumentException if size <= 0.
+     */
+    public IntMatrix(final int size, final int dimension) throws IllegalArgumentException {
+        if (size < 0) {
+            throw new IllegalArgumentException("size <= 0");
+        }
+        this.matrix = new int[(int) Math.pow(size, dimension)];
+        this.dimension = dimension;
+        this.size = size;
+    }
 
-	/**
-	 * Get the integer at the specified index.
-	 *
-	 * @param index the index.
-	 * @return the integer contained in the matrix at the specified index or null if no element is
-	 *         at the specified index.
-	 * @throws ArrayIndexOutOfBoundsException if index.length != dimension and for all i 0 <=
-	 *             index[i] < size does not hold.
-	 */
-	public final int get(final int[] index) throws ArrayIndexOutOfBoundsException {
-		return this.matrix[index(index)];
-	}
+    /**
+     * Get the integer at the specified index.
+     *
+     * @param index the index.
+     * @return the integer contained in the matrix at the specified index or null if no element is
+     *     at the specified index.
+     * @throws ArrayIndexOutOfBoundsException if index.length != dimension and for all i 0 <=
+     *                                        index[i] < size does not hold.
+     */
+    public final int get(final int[] index) throws ArrayIndexOutOfBoundsException {
+        return this.matrix[index(index)];
+    }
 
-	/**
-	 * Put a new value at a specified index.
-	 *
-	 * @param index the index.
-	 * @param value the integer value to put.
-	 * @throws ArrayIndexOutOfBoundsException if index.length != dimension and for all i 0 <=
-	 *             index[i] < size does not hold.
-	 */
-	public final void put(final int[] index, final int value) throws ArrayIndexOutOfBoundsException {
-		this.matrix[index(index)] = value;
-	}
+    /**
+     * Put a new value at a specified index.
+     *
+     * @param index the index.
+     * @param value the integer value to put.
+     * @throws ArrayIndexOutOfBoundsException if index.length != dimension and for all i 0 <=
+     *                                        index[i] < size does not hold.
+     */
+    public final void put(final int[] index, final int value) throws ArrayIndexOutOfBoundsException {
+        this.matrix[index(index)] = value;
+    }
 
-	/**
-	 * Increment the value at a specified index.
-	 *
-	 * @param index the index.
-	 * @throws ArrayIndexOutOfBoundsException if index.length != dimension and for all i 0 <=
-	 *             index[i] < size does not hold.
-	 */
-	public final void increment(final int[] index) throws ArrayIndexOutOfBoundsException {
-		this.matrix[index(index)]++;
-	}
+    /**
+     * Increment the value at a specified index.
+     *
+     * @param index the index.
+     * @throws ArrayIndexOutOfBoundsException if index.length != dimension and for all i 0 <=
+     *                                        index[i] < size does not hold.
+     */
+    public final void increment(final int[] index) throws ArrayIndexOutOfBoundsException {
+        this.matrix[index(index)]++;
+    }
 
-	/**
-	 * Set all the value of the matrix to 0.
-	 */
-	public final void zero() {
-		for (int i = this.matrix.length - 1; i >= 0; i--) {
-			this.matrix[i] = 0;
-		}
-	}
+    /**
+     * Set all the value of the matrix to 0.
+     */
+    public final void zero() {
+        for (int i = this.matrix.length - 1; i >= 0; i--) {
+            this.matrix[i] = 0;
+        }
+    }
 
-	/**
-	 * Return the dimension of the matrix
-	 *
-	 * @return the dimension of the matrix.
-	 */
-	public int getDimension() {
-		return this.dimension;
-	}
+    /**
+     * Return the dimension of the matrix
+     *
+     * @return the dimension of the matrix.
+     */
+    public int getDimension() {
+        return this.dimension;
+    }
 
-	/**
-	 * Return the size of the matrix.
-	 *
-	 * @return size the size of the matrix.
-	 */
-	public final int getSize() {
-		return this.size;
-	}
+    /**
+     * Return the size of the matrix.
+     *
+     * @return size the size of the matrix.
+     */
+    public final int getSize() {
+        return this.size;
+    }
 
-	/**
-	 * Return from the a specified index the index in the internal representation.
-	 *
-	 * @param index the index to convert.
-	 * @return the index in the internal representation.
-	 * @throws ArrayIndexOutOfBoundsException if index.length != dimension and for all i 0 <=
-	 *             index[i] < size does not hold.
-	 */
-	private final int index(final int[] index) throws ArrayIndexOutOfBoundsException {
-		if (index.length != this.dimension)
-			throw new ArrayIndexOutOfBoundsException();
-		for (int i = 0; i < dimension; i++) {
-			if ((index[i] < 0) || (index[i] > size)) {
-				throw new ArrayIndexOutOfBoundsException();
-			}
-		}
-		// conversion E(i * t^r)
-		int acces = 0;
-		for (int i = index.length; i > 0; i--) {
-			acces += index[i - 1] * (int) Math.pow(size, (dimension - i));
-		}
-		return acces;
-	}
+    /**
+     * Return from the a specified index the index in the internal representation.
+     *
+     * @param index the index to convert.
+     * @return the index in the internal representation.
+     * @throws ArrayIndexOutOfBoundsException if index.length != dimension and for all i 0 <=
+     *                                        index[i] < size does not hold.
+     */
+    private final int index(final int[] index) throws ArrayIndexOutOfBoundsException {
+        if (index.length != this.dimension) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        for (int i = 0; i < dimension; i++) {
+            if ((index[i] < 0) || (index[i] > size)) {
+                throw new ArrayIndexOutOfBoundsException();
+            }
+        }
+        // conversion E(i * t^r)
+        int acces = 0;
+        for (int i = index.length; i > 0; i--) {
+            acces += index[i - 1] * (int) Math.pow(size, (dimension - i));
+        }
+        return acces;
+    }
 }
--- src/main/java/fr/uga/pddl4j/encoding/IntOp.java
+++ src/main/java/fr/uga/pddl4j/encoding/IntOp.java
@@ -19,11 +19,11 @@
 
 package fr.uga.pddl4j.encoding;
 
-import java.util.Arrays;
-
 import fr.uga.pddl4j.parser.Connective;
 import fr.uga.pddl4j.util.AbstractCodedOp;
 import fr.uga.pddl4j.util.IntExp;
+
+import java.util.Arrays;
 
 /**
  * This class implements an operator. This class is used to store compact representation of operator
@@ -34,140 +34,145 @@ import fr.uga.pddl4j.util.IntExp;
  */
 final class IntOp extends AbstractCodedOp {
 
-	/**
-	 * The expression that represents the preconditions of the operator.
-	 */
-	private IntExp preconditions;
+    /**
+     * The expression that represents the preconditions of the operator.
+     */
+    private IntExp preconditions;
 
-	/**
-	 * The expression that represents the effect of the operator.
-	 */
-	private IntExp effects;
+    /**
+     * The expression that represents the effect of the operator.
+     */
+    private IntExp effects;
 
-	/**
-	 * Create a new operator from a specified operator. This constructor create a deep copy of the
-	 * specified operator.
-	 *
-	 * @param other the other operator.
-	 * @throws NullPointerException if other == null.
-	 */
-	public IntOp(final IntOp other) {
-		super(other.getName(),
-				Arrays.copyOf(other.parameters, other.getArity()),
-				Arrays.copyOf(other.instantiations, other.getArity()));
-		this.setDummy(other.isDummy());
-		this.preconditions = new IntExp(other.getPreconditions());
-		this.effects = new IntExp(other.getEffects());
-	}
+    /**
+     * Create a new operator from a specified operator. This constructor create a deep copy of the
+     * specified operator.
+     *
+     * @param other the other operator.
+     * @throws NullPointerException if other == null.
+     */
+    public IntOp(final IntOp other) {
+        super(other.getName(),
+            Arrays.copyOf(other.parameters, other.getArity()),
+            Arrays.copyOf(other.instantiations, other.getArity()));
+        this.setDummy(other.isDummy());
+        this.preconditions = new IntExp(other.getPreconditions());
+        this.effects = new IntExp(other.getEffects());
+    }
 
-	/**
-	 * Create a new operator with a specified name.
-	 *
-	 * @param name the name of the operator.
-	 * @param arity the arity of the operator.
-	 * @throws NullPointerException if name == null.
-	 * @throws IllegalArgumentException if arity < 0.
-	 */
-	public IntOp(final String name, final int arity) throws IllegalArgumentException,
-			NullPointerException {
-		super(name, arity);
-		if (arity < 0)
-			throw new IllegalArgumentException("arity < 0");
-		this.parameters = new int[arity];
-		Arrays.fill(this.parameters, -1);
-		this.instantiations = new int[arity];
-		Arrays.fill(this.instantiations, -1);
-		this.preconditions = new IntExp(Connective.OR);
-		this.effects = new IntExp(Connective.OR);
-	}
+    /**
+     * Create a new operator with a specified name.
+     *
+     * @param name  the name of the operator.
+     * @param arity the arity of the operator.
+     * @throws NullPointerException     if name == null.
+     * @throws IllegalArgumentException if arity < 0.
+     */
+    public IntOp(final String name, final int arity) throws IllegalArgumentException,
+        NullPointerException {
+        super(name, arity);
+        if (arity < 0) {
+            throw new IllegalArgumentException("arity < 0");
+        }
+        this.parameters = new int[arity];
+        Arrays.fill(this.parameters, -1);
+        this.instantiations = new int[arity];
+        Arrays.fill(this.instantiations, -1);
+        this.preconditions = new IntExp(Connective.OR);
+        this.effects = new IntExp(Connective.OR);
+    }
 
-	/**
-	 * Return if the operator is ready instantiate with the specified value.
-	 *
-	 * @param value the value.
-	 * @return <code>true</code> if the operator is ready instantiate with the specified value; <code>false</code> otherwise.
-	 */
-	public final boolean isAlreadyInstantiatedWith(final int value) {
-		int i = 0;
-		boolean instantiated = false;
-		while (i < this.instantiations.length && !instantiated) {
-			if (this.instantiations[i] == value)
-				instantiated = true;
-			i++;
-		}
-		return instantiated;
-	}
+    /**
+     * Return if the operator is ready instantiate with the specified value.
+     *
+     * @param value the value.
+     * @return <code>true</code> if the operator is ready instantiate with the specified value; <code>false</code>
+     *     otherwise.
+     */
+    public final boolean isAlreadyInstantiatedWith(final int value) {
+        int i = 0;
+        boolean instantiated = false;
+        while (i < this.instantiations.length && !instantiated) {
+            if (this.instantiations[i] == value) {
+                instantiated = true;
+            }
+            i++;
+        }
+        return instantiated;
+    }
 
-	/**
-	 * Return the preconditions of the operator.
-	 *
-	 * @return the preconditions of the operator.
-	 */
-	public final IntExp getPreconditions() {
-		return this.preconditions;
-	}
+    /**
+     * Return the preconditions of the operator.
+     *
+     * @return the preconditions of the operator.
+     */
+    public final IntExp getPreconditions() {
+        return this.preconditions;
+    }
 
-	/**
-	 * Set the precondition of the operator.
-	 *
-	 * @param preconditions the preconditions to set.
-	 * @throws NullPointerException if preconditions == null.
-	 */
-	public final void setPreconditions(final IntExp preconditions) {
-		if (preconditions == null)
-			throw new NullPointerException("preconditions == null");
-		this.preconditions = preconditions;
-	}
+    /**
+     * Set the precondition of the operator.
+     *
+     * @param preconditions the preconditions to set.
+     * @throws NullPointerException if preconditions == null.
+     */
+    public final void setPreconditions(final IntExp preconditions) {
+        if (preconditions == null) {
+            throw new NullPointerException("preconditions == null");
+        }
+        this.preconditions = preconditions;
+    }
 
-	/**
-	 * Return the effects of the operator.
-	 *
-	 * @return the effects of the operator.
-	 */
-	public final IntExp getEffects() {
-		return this.effects;
-	}
+    /**
+     * Return the effects of the operator.
+     *
+     * @return the effects of the operator.
+     */
+    public final IntExp getEffects() {
+        return this.effects;
+    }
 
-	/**
-	 * Set the new effects of the operator.
-	 *
-	 * @param effects the effects to set
-	 * @throws NullPointerException if preconditions == null.
-	 */
-	public final void setEffects(final IntExp effects) {
-		if (effects == null)
-			throw new NullPointerException("effects == null");
-		this.effects = effects;
-	}
+    /**
+     * Set the new effects of the operator.
+     *
+     * @param effects the effects to set
+     * @throws NullPointerException if preconditions == null.
+     */
+    public final void setEffects(final IntExp effects) {
+        if (effects == null) {
+            throw new NullPointerException("effects == null");
+        }
+        this.effects = effects;
+    }
 
-	/**
-	 * Return if the operator is equal to another object.
-	 *
-	 * @param obj the other object.
-	 * @return <code>true</code> if the specified object is an instance of the class Op and
-	 *         it has the same name and instantiated parameters; otherwise <code>false</code>.
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	public boolean equals(final Object obj) {
-		if (obj != null && obj instanceof IntOp) {
-			final IntOp other = (IntOp) obj;
-			return this.name.equals(other.name) && this.instantiations.equals(other.instantiations);
-		}
-		return false;
-	}
+    /**
+     * Return if the operator is equal to another object.
+     *
+     * @param obj the other object.
+     * @return <code>true</code> if the specified object is an instance of the class Op and
+     *     it has the same name and instantiated parameters; otherwise <code>false</code>.
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    public boolean equals(final Object obj) {
+        if (obj != null && obj instanceof IntOp) {
+            final IntOp other = (IntOp) obj;
+            return this.name.equals(other.name) && this.instantiations.equals(other.instantiations);
+        }
+        return false;
+    }
 
-	/**
-	 * Return the hash code value of the operator.
-	 *
-	 * @return the hash code value of the operator.
-	 * @see java.lang.Object#hashCode()
-	 */
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + instantiations.hashCode();
-		result = prime * result + name.hashCode();
-		return result;
-	}
+    /**
+     * Return the hash code value of the operator.
+     *
+     * @return the hash code value of the operator.
+     * @see java.lang.Object#hashCode()
+     */
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + instantiations.hashCode();
+        result = prime * result + name.hashCode();
+        return result;
+    }
 
 }
