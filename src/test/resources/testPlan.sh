393de7f5a0f5812c691c3dc349e07cb7b598c83c
cedric <cedric.gerard@yeastlab.fr>
2016-02-23 11:55:33 +0100

    update multiple plans testing script

--- src/test/resources/testPlan.sh
+++ src/test/resources/testPlan.sh
@@ -1,13 +1,56 @@
 #!/bin/bash
 
+VERSION=3.0.4-SNAPSHOT
+PROBLEM=""
+TYPE=""
 
+echo "### Execute 16 HSP planifications on blocks world problems"
+TYPE=blocksworld
 for ((i=1 ; i<17 ; i++))
     do
-    if [ $i -lt 10 ]
-        then ./gradlew run -PArgs="-o","pddl/blocksworld/domain.pddl","-f","pddl/blocksworld/p0$i.pddl" -x test -PnoCheckstyle
+    if [ ${i} -lt 10 ]
+        then PROBLEM=p0${i}.pddl
     else
-       ./gradlew run -PArgs="-o","pddl/blocksworld/domain.pddl","-f","pddl/blocksworld/p$i.pddl" -x test -PnoCheckstyle
+        PROBLEM=p${i}.pddl
     fi
+    java -javaagent:build/libs/pddl4j-${VERSION}.jar -server -Xms2048m -Xmx2048m fr.uga.pddl4j.planners.hsp.HSP -o pddl/${TYPE}/domain.pddl -f pddl/${TYPE}/${PROBLEM}
 done
+
+echo "### Execute 4 HSP planifications on depots problems"
+TYPE=depots
+for ((i=1 ; i<5 ; i++))
+    do
+     if [ ${i} -lt 10 ]
+        then PROBLEM=p0${i}.pddl
+    else
+        PROBLEM=p${i}.pddl
+    fi
+    java -javaagent:build/libs/pddl4j-${VERSION}.jar -server -Xms2048m -Xmx2048m fr.uga.pddl4j.planners.hsp.HSP -o pddl/${TYPE}/domain.pddl -f pddl/${TYPE}/${PROBLEM}
+done
+
+echo "### Execute 5 HSP planifications on gripper problems"
+TYPE=gripper
+for ((i=1 ; i<6 ; i++))
+    do
+     if [ ${i} -lt 10 ]
+        then PROBLEM=p0${i}.pddl
+    else
+        PROBLEM=p${i}.pddl
+    fi
+    java -javaagent:build/libs/pddl4j-${VERSION}.jar -server -Xms2048m -Xmx2048m fr.uga.pddl4j.planners.hsp.HSP -o pddl/${TYPE}/domain.pddl -f pddl/${TYPE}/${PROBLEM}
+done
+
+echo "### Execute 11 HSP planifications on logistics problems"
+TYPE=logistics
+for ((i=1 ; i<12 ; i++))
+    do
+     if [ ${i} -lt 10 ]
+        then PROBLEM=p0${i}.pddl
+    else
+        PROBLEM=p${i}.pddl
+    fi
+    java -javaagent:build/libs/pddl4j-${VERSION}.jar -server -Xms2048m -Xmx2048m fr.uga.pddl4j.planners.hsp.HSP -o pddl/${TYPE}/domain.pddl -f pddl/${TYPE}/${PROBLEM}
+done
+
 
 exit 0
