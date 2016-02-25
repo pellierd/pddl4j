7c3cdf6a8cce44fb76d22dd13f89eaa4c449b748
cedric <cedric.gerard@yeastlab.fr>
2016-02-25 10:41:24 +0100

    improve double comparison

--- src/main/java/fr/uga/pddl4j/util/IntExp.java
+++ src/main/java/fr/uga/pddl4j/util/IntExp.java
@@ -260,7 +260,7 @@ public class IntExp implements Serializable {
             return this.connective.equals(other.connective)
                 && this.predicate == other.predicate
                 && Arrays.equals(this.arguments, other.arguments)
-                && this.value == other.value
+                && Double.compare(this.value,other.value) == 0
                 && this.variable == other.variable
                 && this.type == other.type
                 && this.children.equals(other.children);
