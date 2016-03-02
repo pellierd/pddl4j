0ab6090086ea9e0e0b97a6698c4be467634c8c63
cedric <cedric.gerard@yeastlab.fr>
2016-03-02 15:37:55 +0100

    little typo in comment

--- src/main/java/fr/uga/pddl4j/heuristics/relaxation/HeuristicToolKit.java
+++ src/main/java/fr/uga/pddl4j/heuristics/relaxation/HeuristicToolKit.java
@@ -31,7 +31,7 @@ import fr.uga.pddl4j.encoding.CodedProblem;
 public final class HeuristicToolKit {
 
     /**
-     * Private constructor just for prevent user to instantiate this class
+     * Private constructor just for prevent user to instantiate this class.
      */
     private HeuristicToolKit(){}
 
