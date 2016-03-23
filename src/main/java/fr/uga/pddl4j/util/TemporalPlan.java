91020b7cd87c6a4faa5115568f4e04ff17ec1ff5
pellierd <damien.pellier@imag.fr>
2016-03-23 11:49:48 +0100

    Add the class TemporalPlan

---
+++ src/main/java/fr/uga/pddl4j/util/TemporalPlan.java
@@ -0,0 +1,171 @@
+/*
+ * Copyright (c) 2016 by Damien Pellier <Damien.Pellier@imag.fr>.
+ *
+ * This file is part of PDDL4J library.
+ *
+ * PDDL4J is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * PDDL4J is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with PDDL4J.  If not, see
+ * <http://www.gnu.org/licenses/>
+ */
+
+package fr.uga.pddl4j.util;
+
+import java.util.*;
+
+
+/**
+ * This class implements a temporal plan.
+ *
+ * @author D. Pellier
+ * @version 1.0 - 23.03.2016
+ *
+ * @since 3.0
+ */
+public class TemporalPlan extends AbstractPlan{
+
+    /**
+     * The list used to store the actions contained in the plan.
+     */
+    private Map<Integer, Set<BitOp>> actions;
+
+    /**
+     * Creates a new empty temporal plan.
+     *
+     * @see AbstractPlan#AbstractPlan()
+     */
+    public TemporalPlan() {
+        super();
+        this.actions = new TreeMap<>();
+    }
+
+    /**
+     * Creates a new temporal plan from an other. This constructor creates a deep copy of the specified plan.
+     *
+     * @param other the other plan.
+     * @see AbstractPlan#AbstractPlan(Plan)
+     */
+    public TemporalPlan(final Plan other) {
+        super(other);
+    }
+
+    /**
+     * Returns the makespan of the plan. The makespan of a sequential plan is its size.
+     *
+     * @return the makespan of the plan.
+     * @see Plan#size()
+     */
+    @Override
+    public final double makespan() {
+        //TO DO
+        return 0.0;
+    }
+
+    /**
+     * Returns the list of actions contained in the plan ordered depending on their time specifier
+     *
+     * @return the ordered set of actions of the plan.
+     * @see Plan#actions()
+     */
+    @Override
+    public final List<BitOp> actions() {
+        final List<BitOp> acts = new ArrayList<>();
+        this.actions.forEach(acts::addAll);
+        return acts;
+    }
+
+    /**
+     * Returns the set of time specifiers used in this plan.
+     *
+     * @return the set of time specifiers used in this plan.
+     * @see Plan#timeSpecifiers()
+     */
+    @Override
+    public final Set<Integer> timeSpecifiers() {
+        return this.actions.keySet();
+    }
+
+    /**
+     * Returns the set of actions at a specified time specifier.
+     *
+     * @param time the time specifier.
+     * @return the set of actions at a specified time specifier or null if no actions are scheduled in the plan at the
+     *     the time specifier.
+     * @see Plan#getActionSet(int)
+     */
+    @Override
+    public final Set<BitOp> getActionSet(final int time) {
+        return this.actions.get(time);
+    }
+
+    /**
+     * Adds an action at a specified time specifier in the plan.
+     *
+     * @param action the action to add.
+     * @param time the time specifier of the action in the plan.
+     * @return <code>true</code> if the action was added; <code>false</code> otherwise.
+     * @see Plan#add(int, BitOp)
+     */
+    @Override
+    public final boolean add(final int time, final BitOp action) {
+        Set<BitOp> set = this.actions.get(time);
+        if (set == null) {
+            set = new HashSet<BitOp>();
+            this.actions.put(time, set);
+        }
+        return set.add(action);
+    }
+
+    /**
+     * Removes an action at a specified time specifier of the plan.
+     *
+     * @param action the action to remove.
+     * @param time the time specifier of the action in the plan to remove.
+     * @see Plan#remove(int, BitOp)
+     */
+    @Override
+    public final boolean remove(final int time, final BitOp action) {
+        return this.actions.get(time).remove(action);
+    }
+
+    /**
+     * Removes all the actions at a specified time specifier of the plan.
+     *
+     * @param time the time specifier of the actions in the plan to remove.
+     * @see Plan#remove(int)
+     */
+    @Override
+    public final boolean remove(final int time) {
+        return this.actions.remove(time) != null;
+    }
+
+    /**
+     * Returns if an action is contained in the plan at a specified time specifier.
+     *
+     * @param time the time specifier.
+     * @param action the action.
+     * @return <code>true</code> if the specified action is contained in the plan at the specified time specifier;
+     *     <code>false</code> otherwise.
+     * @see Plan#contains(int, BitOp)
+     */
+    @Override
+    public final boolean contains(final int time, final BitOp action) {
+        final Set<BitOp> set = this.actions.get(time);
+        return set != null && set.contains(action);
+    }
+
+    /**
+     * Removes all the actions of the plan.
+     *
+     * @see Plan#clear()
+     */
+    @Override
+    public final void clear() {
+        this.actions.clear();
+    }
+
+}
