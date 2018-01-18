143980579442557db96939ab0506a760126cb574
Emmanuel Hermellin <emmanuel.hermellin@univ-grenoble-alpes.fr>
2018-01-18 16:29:02 +0100

    Add Common class which will contain common functions between planners

---
+++ src/main/java/fr/uga/pddl4j/planners/common/Common.java
@@ -0,0 +1,54 @@
+package fr.uga.pddl4j.planners.common;
+
+public final class Common {
+
+    /**
+     * This method print the usage of the command-line planner.
+     */
+    public static StringBuilder printUsage() {
+
+        final StringBuilder strb = new StringBuilder();
+
+        strb.append("\nusage of hsp:\n")
+            .append("OPTIONS   DESCRIPTIONS\n")
+            .append("-o <str>    operator file name\n")
+            .append("-f <str>    fact file name\n")
+            .append("-w <num>    the weight used in the a star seach (preset: 1)\n")
+            .append("-t <num>    specifies the maximum CPU-time in seconds (preset: 300)\n")
+            .append("-u <num>    specifies the heuristic to used (preset: 0)\n")
+            .append("     0      ff heuristic\n")
+            .append("     1      sum heuristic\n")
+            .append("     2      sum mutex heuristic\n")
+            .append("     3      adjusted sum heuristic\n")
+            .append("     4      adjusted sum 2 heuristic\n")
+            .append("     5      adjusted sum 2M heuristic\n")
+            .append("     6      combo heuristic\n")
+            .append("     7      max heuristic\n")
+            .append("     8      set-level heuristic\n")
+            .append("-i <num>    run-time information level (preset: 1)\n")
+            .append("     0      nothing\n")
+            .append("     1      info on action number, search and search\n")
+            .append("     2      1 + info on problem constants, types and predicates\n")
+            .append("     3      1 + 2 + loaded operators, initial and goal state\n")
+            .append("     4      1 + predicates and their inertia status\n")
+            .append("     5      1 + 4 + goal state and operators with unary inertia encoded\n")
+            .append("     6      1 + actions, initial and goal state after expansion of variables\n")
+            .append("     7      1 + final domain representation\n")
+            .append("     8      line representation:\n")
+            .append("               - problem name\n")
+            .append("               - number of operators\n")
+            .append("               - number of facts\n")
+            .append("               - parsing time in seconds\n")
+            .append("               - encoding time in seconds\n")
+            .append("               - searching time in seconds\n")
+            .append("               - total time in seconds\n")
+            .append("               - memory used for problem representation in MBytes\n")
+            .append("               - memory used for searching in MBytes\n")
+            .append("               - total memory used in MBytes\n")
+            .append("               - length of the solution plan\n")
+            .append("-s <bool>   generate statistics or not (preset: true)\n")
+            .append("-h          print this message\n\n");
+
+        return strb;
+    }
+}
