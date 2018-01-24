ed1edcfe20671245e54280433d069ddbc5d0843d
Emmanuel Hermellin <emmanuel.hermellin@univ-grenoble-alpes.fr>
2018-01-24 13:40:04 +0100

    Extract parseArgument from FF and HSP

--- src/main/java/fr/uga/pddl4j/planners/hsp/HSP.java
+++ src/main/java/fr/uga/pddl4j/planners/hsp/HSP.java
@@ -315,7 +315,7 @@ public final class HSP extends AbstractPlanner {
 
         try {
             // Parse the command line
-            final Properties arguments = HSP.parseArguments(args);
+            final Properties arguments = CommonPlanner.parseArguments(args,LOGGER,HSP.getDefaultArguments());
             final File domain = (File) arguments.get(CommonPlanner.Argument.DOMAIN);
             final File problem = (File) arguments.get(CommonPlanner.Argument.PROBLEM);
             final int traceLevel = (Integer) arguments.get(CommonPlanner.Argument.TRACE_LEVEL);
@@ -467,91 +467,6 @@ public final class HSP extends AbstractPlanner {
             System.exit(1);
         }
     }
-
-    /**
-     * This method parse the command line and return the arguments.
-     *
-     * @param args the arguments from the command line.
-     * @return The arguments of the planner.
-     */
-    private static Properties parseArguments(String[] args) throws FileException {
-        final Properties arguments = HSP.getDefaultArguments();
-        try {
-            for (int i = 0; i < args.length; i += 2) {
-                if ("-o".equalsIgnoreCase(args[i]) && ((i + 1) < args.length)) {
-                    if (!new File(args[i + 1]).exists()) {
-                        LOGGER.trace("operators file does not exist: " + args[i + 1] + "\n");
-                    }
-                    arguments.put(CommonPlanner.Argument.DOMAIN, new File(args[i + 1]));
-                } else if ("-f".equalsIgnoreCase(args[i]) && ((i + 1) < args.length)) {
-                    if (!new File(args[i + 1]).exists()) {
-                        LOGGER.trace("facts file does not exist: " + args[i + 1] + "\n");
-                    }
-                    arguments.put(CommonPlanner.Argument.PROBLEM, new File(args[i + 1]));
-                } else if ("-t".equalsIgnoreCase(args[i]) && ((i + 1) < args.length)) {
-                    final int cpu = Integer.parseInt(args[i + 1]) * 1000;
-                    if (cpu < 0) {
-                        LOGGER.trace(CommonPlanner.printUsage());
-                    }
-                    arguments.put(CommonPlanner.Argument.TIMEOUT, cpu);
-                } else if ("-u".equalsIgnoreCase(args[i]) && ((i + 1) < args.length)) {
-                    final int heuristic = Integer.parseInt(args[i + 1]);
-                    if (heuristic < 0 || heuristic > 8) {
-                        LOGGER.trace(CommonPlanner.printUsage());
-                    }
-                    if (heuristic == 0) {
-                        arguments.put(CommonPlanner.Argument.HEURISTIC, Heuristic.Type.FAST_FORWARD);
-                    } else if (heuristic == 1) {
-                        arguments.put(CommonPlanner.Argument.HEURISTIC, Heuristic.Type.SUM);
-                    } else if (heuristic == 2) {
-                        arguments.put(CommonPlanner.Argument.HEURISTIC, Heuristic.Type.SUM_MUTEX);
-                    } else if (heuristic == 3) {
-                        arguments.put(CommonPlanner.Argument.HEURISTIC, Heuristic.Type.AJUSTED_SUM);
-                    } else if (heuristic == 4) {
-                        arguments.put(CommonPlanner.Argument.HEURISTIC, Heuristic.Type.AJUSTED_SUM2);
-                    } else if (heuristic == 5) {
-                        arguments.put(CommonPlanner.Argument.HEURISTIC, Heuristic.Type.AJUSTED_SUM2M);
-                    } else if (heuristic == 6) {
-                        arguments.put(CommonPlanner.Argument.HEURISTIC, Heuristic.Type.COMBO);
-                    } else if (heuristic == 7) {
-                        arguments.put(CommonPlanner.Argument.HEURISTIC, Heuristic.Type.MAX);
-                    } else {
-                        arguments.put(CommonPlanner.Argument.HEURISTIC, Heuristic.Type.SET_LEVEL);
-                    }
-                } else if ("-w".equalsIgnoreCase(args[i]) && ((i + 1) < args.length)) {
-                    final double weight = Double.parseDouble(args[i + 1]);
-                    if (weight < 0) {
-                        LOGGER.trace(CommonPlanner.printUsage());
-                    }
-                    arguments.put(CommonPlanner.Argument.WEIGHT, weight);
-                } else if ("-i".equalsIgnoreCase(args[i]) && ((i + 1) < args.length)) {
-                    final int level = Integer.parseInt(args[i + 1]);
-                    if (level < 0) {
-                        LOGGER.trace(CommonPlanner.printUsage());
-                    }
-                    arguments.put(CommonPlanner.Argument.TRACE_LEVEL, level);
-                } else if ("-s".equalsIgnoreCase(args[i]) && ((i + 1) < args.length)) {
-                    final boolean isStatUsed = Boolean.parseBoolean(args[i + 1]);
-                    arguments.put(CommonPlanner.Argument.STATISTICS, isStatUsed);
-                } else {
-                    LOGGER.trace("\nUnknown argument for \"" + args[i] + "\" or missing value\n");
-                    LOGGER.trace(CommonPlanner.printUsage());
-                    throw new FileException("Unknown arguments: " + args[i]);
-                }
-            }
-            if (arguments.get(CommonPlanner.Argument.DOMAIN) == null || arguments.get(CommonPlanner.Argument.PROBLEM) == null) {
-                LOGGER.trace("\nMissing DOMAIN or PROBLEM\n");
-                LOGGER.trace(CommonPlanner.printUsage());
-                throw new FileException("Missing domain or problem");
-            }
-        } catch (RuntimeException runExp) {
-            LOGGER.trace("\nError when parsing arguments\n");
-            LOGGER.trace(CommonPlanner.printUsage());
-            throw runExp;
-        }
-        return arguments;
-    }
-
 
     /**
      * This method return the default arguments of the planner.
