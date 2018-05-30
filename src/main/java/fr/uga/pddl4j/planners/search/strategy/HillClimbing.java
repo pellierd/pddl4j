cbac562d2e2f1e0c857b9421cd0a656df9432432
Emmanuel Hermellin <emmanuel.hermellin@univ-grenoble-alpes.fr>
2018-05-30 09:05:34 +0200

    Add HillClimbing search strategy, add in EHC MemoryAgent control

--- src/main/java/fr/uga/pddl4j/planners/search/strategy/HillClimbing.java
+++ src/main/java/fr/uga/pddl4j/planners/search/strategy/HillClimbing.java
@@ -1,22 +1,3 @@
-/*
- * Copyright (c) 2010 by Damien Pellier <Damien.Pellier@imag.fr>.
- *
- * This file is part of PDDL4J library.
- *
- * PDDL4J is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * PDDL4J is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with PDDL4J.  If not, see <http://www.gnu.org/licenses/>
- */
-
 package fr.uga.pddl4j.planners.search.strategy;
 
 import fr.uga.pddl4j.encoding.CodedProblem;
@@ -29,27 +10,17 @@ import fr.uga.pddl4j.util.BitOp;
 import fr.uga.pddl4j.util.BitState;
 import fr.uga.pddl4j.util.MemoryAgent;
 
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Objects;
 
-/**
- * This class implements Enforced Hill Climbing planner.
- *
- * @author Samuel Aaron Boyd
- * @author E. Hermellin
- * @version 2.0 - 24.01.2018
- */
-public class EnforcedHillClimbing {
-
-    /**
-     * The time needed to search a solution node.
-     */
-    private static long searchingTime;
+public class HillClimbing {
 
     /**
      * Creates a new planner.
      */
-    private EnforcedHillClimbing() {
+    private HillClimbing() {
     }
 
     /**
@@ -61,52 +32,50 @@ public class EnforcedHillClimbing {
      */
     public static Node searchSolutionNode(final Planner planner, final CodedProblem pb) {
         Objects.requireNonNull(pb);
-        searchingTime = 0;
-        return EnforcedHillClimbing.enforcedHillClimbing(planner, pb);
+        return HillClimbing.hillClimbing(planner, pb);
     }
 
     /**
-     * The enforced hill climbing algorithm. Solves the planning problem and returns the solution's node.
+     * The hill climbing algorithm. Solves the planning problem and returns the solution's node.
      *
      * @param planner the planner used to solve the problem
-     * @param problem the coded problem to solve.
-     * @return the solution node or null.
+     * @param pb      the problem to solve.
+     * @return the solution node.
      */
-    private static Node enforcedHillClimbing(final Planner planner, final CodedProblem problem) {
-        final long begin = System.currentTimeMillis();
-        final Heuristic heuristic = HeuristicToolKit.createHeuristic(planner.getHeuristicType(), problem);
+    private static Node hillClimbing(final Planner planner, final CodedProblem pb) {
         final LinkedList<Node> openList = new LinkedList<>();
-        final int timeout = planner.getTimeout();
+        final Heuristic heuristic = HeuristicToolKit.createHeuristic(planner.getHeuristicType(), pb);
+        long searchingTime = 0;
+        double bestHeuristic = Integer.MAX_VALUE;
 
-        BitState init = new BitState(problem.getInit());
-        Node root = new Node(init, null, 0, 0, heuristic.estimate(init, problem.getGoal()));
+        BitState init = new BitState(pb.getInit());
+        Node root = new Node(init, null, 0, 0, heuristic.estimate(init, pb.getGoal()));
         openList.add(root);
-
-        double bestHeuristic = root.getHeuristic();
 
         Node solution = null;
         boolean deadEndFree = true;
 
-        while (!openList.isEmpty() && solution == null && deadEndFree && searchingTime < timeout) {
+        final int timeout = planner.getTimeout();
+        final long begin = System.currentTimeMillis();
+        while (!openList.isEmpty() && solution == null
+            && deadEndFree && searchingTime < timeout) {
+
             final Node currentState = openList.pop();
-            final LinkedList<Node> successors = EnforcedHillClimbing.getSuccessors(currentState, problem, heuristic);
+            final LinkedList<Node> successors = HillClimbing.getSuccessors(currentState, pb, heuristic);
             deadEndFree = !successors.isEmpty();
 
-            while (!successors.isEmpty() && solution == null) {
-                final Node successor = successors.pop();
-                final double heuristicSuccessor = successor.getHeuristic();
-                if (heuristicSuccessor == 0.0) {
-                    solution = successor;
-                }
-                if (heuristicSuccessor < bestHeuristic) {
-                    successors.clear();
-                    openList.clear();
-                    bestHeuristic = heuristicSuccessor;
-                }
+            final Node successor = HillClimbing.popBestNode(successors, bestHeuristic);
+            if (successor.satisfy(pb.getGoal())) {
+                solution = successor;
+            } else {
+                successors.clear();
+                openList.clear();
                 openList.addLast(successor);
+                if (successor.getHeuristic() <= bestHeuristic) {
+                    bestHeuristic = successor.getHeuristic();
+                }
             }
 
-            // Take time to compute the searching time
             long end = System.currentTimeMillis();
             searchingTime = end - begin;
         }
@@ -136,12 +105,12 @@ public class EnforcedHillClimbing {
      * @param heuristic the heuristic used.
      * @return the list of successors from the parent node.
      */
-    private static LinkedList<Node> getSuccessors(Node parent, CodedProblem problem, Heuristic heuristic) {
+    private static LinkedList<Node> getSuccessors(final Node parent, final CodedProblem problem,
+                                                  final Heuristic heuristic) {
         final LinkedList<Node> successors = new LinkedList<>();
 
         int index = 0;
         for (BitOp op : problem.getOperators()) {
-            // Test if a specified operator is applicable in the current state
             if (op.isApplicable(parent)) {
                 final BitState nextState = new BitState(parent);
                 nextState.or(op.getCondEffects().get(0).getEffects().getPositive());
@@ -160,5 +129,27 @@ public class EnforcedHillClimbing {
         }
 
         return successors;
+    }
+
+    /**
+     * Return the best node from a list according to the heuristic value.
+     *
+     * @param nodes the list containing nodes.
+     * @return the best node from the nodes' list.
+     */
+    private static Node popBestNode(final Collection<Node> nodes, final double bestHeuristic) {
+        Node node = null;
+        if (!nodes.isEmpty()) {
+            final Iterator<Node> i = nodes.iterator();
+            node = i.next();
+            while (i.hasNext()) {
+                final Node next = i.next();
+                if (next.getHeuristic() < bestHeuristic) {
+                    node = next;
+                }
+            }
+            nodes.remove(node);
+        }
+        return node;
     }
 }
