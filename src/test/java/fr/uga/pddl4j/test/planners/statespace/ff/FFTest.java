91ed50da4ce97da5281526d81917878246f6b815
Emmanuel Hermellin <ehermellin.um2@gmail.com>
2018-11-26 09:06:24 +0100

    clean

--- src/test/java/fr/uga/pddl4j/test/planners/statespace/ff/FFTest.java
+++ src/test/java/fr/uga/pddl4j/test/planners/statespace/ff/FFTest.java
@@ -27,12 +27,9 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
--- src/test/java/fr/uga/pddl4j/test/planners/statespace/generic/GenericPlannerTest.java
+++ src/test/java/fr/uga/pddl4j/test/planners/statespace/generic/GenericPlannerTest.java
@@ -32,12 +32,9 @@ import fr.uga.pddl4j.util.Plan;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
