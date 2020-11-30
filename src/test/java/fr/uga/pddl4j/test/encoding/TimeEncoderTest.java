a950b10c4c8e48e20ea79d482711949884ede402
pellierd <damien.pellier@imag.fr>
2020-11-30 09:36:16 +0100

    Add benchmarcks ipc2006/pipesworld/metric-time-constraints benchmarks

--- src/test/java/fr/uga/pddl4j/test/encoding/TimeEncoderTest.java
+++ src/test/java/fr/uga/pddl4j/test/encoding/TimeEncoderTest.java
@@ -618,9 +618,22 @@ public class TimeEncoderTest {
      * Failure: p01
      * @throws Exception if something went wrong.
      */
-    @Test
+    /*@Test
     public void test_Parser_IPC2006_Pipesworld_Metric_Time() throws Exception {
         final String localTestPath = Tools.PDDL_BENCH_DIR + "ipc2006/pipesworld/metric-time" + File.separator;
+        Assert.assertTrue("missing benchmark [directory: " + localTestPath + "] test skipped !",
+            Tools.isBenchmarkExist(localTestPath));
+        Tools.encodePDDLProblems(localTestPath, TimeEncoderTest.TRACE_LEVEL);
+    }
+
+    /**
+     * Method that executes tests using IPC 2006 Pipesworld metric time constraints benchmarks.
+     * Failure: Constraints not supported
+     * @throws Exception if something went wrong.
+     */
+    @Test
+    public void test_Parser_IPC2006_Pipesworld_Metric_Time_Constraints() throws Exception {
+        final String localTestPath = Tools.PDDL_BENCH_DIR + "ipc2006/pipesworld/metric-time-constraints" + File.separator;
         Assert.assertTrue("missing benchmark [directory: " + localTestPath + "] test skipped !",
             Tools.isBenchmarkExist(localTestPath));
         Tools.encodePDDLProblems(localTestPath, TimeEncoderTest.TRACE_LEVEL);
